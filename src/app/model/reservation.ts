86764076605da1a6b1501f870e6baf27fe9e1599
DavidLapenaGarcia <davidlaga93@gmail.com>
2019-01-25 16:50:24 +0100

    class reservation modified

--- src/app/model/reservation.ts
+++ src/app/model/reservation.ts
@@ -1,73 +1,69 @@
 //una clase siempre debe tener export para poder ser usada
 export class Reservation {
 
-    _id: number;
-    _name: string;
-    _surname: string;
-    _email: string;
-    _phone: string;
-    _reservation: Date;
-    _reservationTime: string;
+     id             : number;
+     name           : string;
+     surname        : string;
+     email          : string;
+     phone          : string;
+     reservation    : Date;
+     reservationTime: string;
 
 
     constructor(pId: number, pName: string, pSurname: string, pEmail: string, pPhone: string,
         pReservation: Date, pReservationTime: string) {
-        this._id = pId;
-        this._name = pName;
-        this._surname = pSurname;
-        this._email = pEmail;
-        this._phone = pPhone;
-        this._reservation = pReservation;
-        this._reservationTime = pReservationTime;
+        this.id              = pId;
+        this.name            = pName;
+        this.surname         = pSurname;
+        this.email           = pEmail;
+        this.phone           = pPhone;
+        this.reservation     = pReservation;
+        this.reservationTime = pReservationTime;
 
     }
 
     getId(): number {
-        return this._id;
+        return this.id;
     }
     getName(): string {
-        return this._name;
+        return this.name;
     }
     getSurname(): string {
-        return this._surname;
+        return this.surname;
     }
     getEmail(): string {
-        return this._email;
+        return this.email;
     }
     getPhone(): string {
-        return this._phone;
+        return this.phone;
     }
     getReservation(): Date {
-        return this._reservation;
+        return this.reservation;
     }
     getReservationTime(): string {
-        return this._reservationTime;
+        return this.reservationTime;
     }
 
     setId(pId: number): void {
-        this._id = pId;
+        this.id = pId;
     }
     setName(pName: string): void {
-        this._name = pName;
+        this.name = pName;
     }
     setSurname(pSurname: string): void {
-        this._surname = pSurname;
+        this.surname = pSurname;
     }
     setEmail(pEmail: string): void {
-        this._email = pEmail;
+        this.email = pEmail;
     }
     setPhone(pPhone: string): void {
-        this._phone = pPhone;
+        this.phone = pPhone;
     }
     setReservation(pReservation: Date): void {
-        this._reservation = pReservation;
+        this.reservation = pReservation;
     }
     setReservationTime(pReservationTime: string): void {
-        this._reservationTime = pReservationTime;
+        this.reservationTime = pReservationTime;
     }
-
-
-
-
 
 }
