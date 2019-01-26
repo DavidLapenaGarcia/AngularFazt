b56c4dc4327d5ccce384cd3b4bd3c9097bab47e4
DavidLapenaGarcia <davidlaga93@gmail.com>
2019-01-26 21:08:45 +0100

    servers component added by CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent }   from './app.component';
 import {ServerComponent}  from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule,
