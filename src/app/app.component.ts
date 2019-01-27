7704c58e71250a669e73a1c2c742783edaf40ae7
DavidLapenaGarcia <davidlaga93@gmail.com>
2019-01-27 19:42:02 +0100

    solved task2 by me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName:string = "";
+  diseableButtton:boolean = false;
+
+  constructor(){
+    this.diseableButtton = false;
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
+  onWritteUser(event: Event){
+    this.userName = (<HTMLInputElement>event.target).value;
+    if(this.userName.length>0){
+      this.diseableButtton = true;
+    }
+  }
+
+  buttonCliked(){
+    this.userName = "";
+    console.log("why?");
+  }
 }
