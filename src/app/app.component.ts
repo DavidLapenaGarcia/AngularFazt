651d114d170aaea402d3176af0070b3fabb95af1
DavidLapenaGarcia <davidlaga93@gmail.com>
2019-01-27 19:51:49 +0100

    return to the original workspace

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-main',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  /*styles: [`
+   h3{color:blue}
+  `]*/
 })
 export class AppComponent {
-  userName:string = "";
-  diseableButtton:boolean = false;
-
-  constructor(){
-    this.diseableButtton = false;
-  }
-
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    
-  }
-  onWritteUser(event: Event){
-    this.userName = (<HTMLInputElement>event.target).value;
-    if(this.userName.length>0){
-      this.diseableButtton = true;
-    }
-  }
-
-  buttonCliked(){
-    this.userName = "";
-    console.log("why?");
-  }
 }
