41f0787b3445d3a378e794e7fcaf4851760e1c23
DavidLapenaGarcia <davidlaga93@gmail.com>
2019-01-27 18:55:22 +0100

    getting and setting data I

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpyNgModuleFactoryLoader } from '@angular/router/testing';
 
 @Component({
   //selector: '[app-servers]', to make it attribute
@@ -10,10 +11,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No serverCrated';
+  serverName: string='';
 
-  constructor() { }
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    }, 2000);
+   }
 
   ngOnInit() {
   }
 
+  onCreateServer(){
+    this.serverCreationStatus="serverCREATED";
+  }  
+  onUpdateServerNane(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
