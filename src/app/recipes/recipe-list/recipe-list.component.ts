ea07df583f2d03b4658a13425140edd844745513
DavidLapenaGarcia <davidlaga93@gmail.com>
2019-01-28 18:04:44 +0100

    Displaied recipes-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from './../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+    recipes: Recipe[] = [
+    new Recipe( 'A test Recipe',
+                'This is a simplr test',
+                'https://www.maxpixel.net/static/photo/1x/Food-Dishes-Eat-Gastronomy-Lunch-Recipe-2760200.jpg'
+                )
+  ];
 
   constructor() { }
 
