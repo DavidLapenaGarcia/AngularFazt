ea6594fca0a7ddb24f413bfdfb35b86ccb54f699
DavidLapenaGarcia <davidlaga93@gmail.com>
2019-01-28 18:17:51 +0100

    Displaing recipes list improved

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,11 +9,15 @@ import { Recipe } from './../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
     recipes: Recipe[] = [
-    new Recipe( 'A test Recipe',
-                'This is a simplr test',
-                'https://www.maxpixel.net/static/photo/1x/Food-Dishes-Eat-Gastronomy-Lunch-Recipe-2760200.jpg'
-                )
-  ];
+              new Recipe( 'A test Recipe',
+                          'This is a simplr test',
+                          'https://www.maxpixel.net/static/photo/1x/Food-Dishes-Eat-Gastronomy-Lunch-Recipe-2760200.jpg'
+                          ),
+              new Recipe( 'A test Recipe2',
+                          'This is a simplr test2',
+                          'https://upload.wikimedia.org/wikipedia/commons/1/11/Pindi-chana-recipe.jpg'
+                          ),
+            ];
 
   constructor() { }
 
