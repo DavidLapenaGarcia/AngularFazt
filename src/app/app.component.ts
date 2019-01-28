a1e2901d1ee7ba7a1b4d644e38abb4c2883541f5
DavidLapenaGarcia <davidlaga93@gmail.com>
2019-01-28 02:55:03 +0100

    start Section 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-main',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-  /*styles: [`
-   h3{color:blue}
-  `]*/
 })
 export class AppComponent {
 }
