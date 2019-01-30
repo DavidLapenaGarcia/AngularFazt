1df27322206a86a3a5dc6d42adb8d54f32315ccb
DavidLapenaGarcia <davidlaga93@gmail.com>
2019-01-30 15:14:51 +0100

    Binding to Custom events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   serverElements = [{type: 'server', name: 'Testserver', content: 'JustContent'}];
+
+  /**
+   * onServerAdded
+   * This function is only executable by a button on app.component
+   */
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+  /**
+   * onBlueprintAdded
+   * This function is only executable by a button on app.component
+   */
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+      this.serverElements.push({
+        type: 'blueprint',
+        name: blueprintData.serverName,
+        content: blueprintData.serverContent
+      });
+  }
 }
