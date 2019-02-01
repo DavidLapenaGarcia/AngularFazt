5955c71c13d783055a189fe726c7da90ecaa0939
DavidLapenaGarcia <davidlaga93@gmail.com>
2019-02-01 04:15:01 +0100

    More on view encapsulation

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,10 +1,15 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Native
 })
+// using ViewEncapsulation.None, Father css naw will go on in this component.
+// using ViewEncapsulation.Native, only if the browser is able to emulate
+// using ViewEncapsulation.Emulaete,predeterminated
+
 export class ServerElementComponent implements OnInit {
   // tslint:disable-next-line:no-input-rename
   @Input('srvElement') element: {type: string, name: string, content: string};
