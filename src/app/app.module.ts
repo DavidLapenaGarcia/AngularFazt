7f6fec3945e09fea2aa2d504e733b975aeff694c
DavidLapenaGarcia <davidlaga93@gmail.com>
2019-02-03 20:34:23 +0100

    Starting Section 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { BasicHightLightDirective } from './basic-highlight/basic-highlight.directive';
-import { BetterhighlightDirective } from './better-highlight/betterhighlight.directive';
-import { UnlessDirective } from './better-highlight/unless.directive';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHightLightDirective,
-    BetterhighlightDirective,
-    UnlessDirective
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
