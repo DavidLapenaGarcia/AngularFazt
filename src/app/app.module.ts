a3d48d03d735d4773f1f0137ec5fdf47f1a521e1
DavidLapenaGarcia <davidlaga93@gmail.com>
2019-02-03 19:52:15 +0100

    Behind a Structural Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BasicHightLightDirective } from './basic-highlight/basic-highlight.directive';
 import { BetterhighlightDirective } from './better-highlight/betterhighlight.directive';
+import { UnlessDirective } from './better-highlight/unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHightLightDirective,
-    BetterhighlightDirective
+    BetterhighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
