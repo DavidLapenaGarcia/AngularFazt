ef8d17420e2f676eb660ffd445bd0e44b174ce8c
DavidLapenaGarcia <davidlaga93@gmail.com>
2019-02-04 02:33:53 +0100

    app/module.ts naw works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { AccountComponent } from './account/account.component';
@@ -16,7 +15,6 @@ import { NewAccountComponent } from './new-account/new-account.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
