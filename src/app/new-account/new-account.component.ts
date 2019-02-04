afbde48a61f59c83f354b5a4ce2d46b5a1f690a2
DavidLapenaGarcia <davidlaga93@gmail.com>
2019-02-04 19:04:37 +0100

    Understand  the Hierarchinal Injector. How much server's instances?

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -6,11 +6,15 @@ import { Component} from '@angular/core';
   /* VIDEO 99.2 Injecting the Logging Service into Components
       To tell how give the service, we add a provider
   */
+ /* VIDEO 102.1 How much SINstance os Service Should It Be?
+      Delete from provaiders, to evit instance a new AccountService,
+      and get the service hereidtaired by app.component
+  */
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService, AccountsService]
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
 
@@ -23,7 +27,6 @@ export class NewAccountComponent {
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
-
   /* VIDEO 99.3  Injecting the Logging Service into Components
     Naw we can use our propierty 'loggingService: LoggingService'.
     So, naw angular are making the instance for as.
