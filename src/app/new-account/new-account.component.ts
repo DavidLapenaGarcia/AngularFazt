5050db952128f3d767d1fbccef87d59fc5822e5f
DavidLapenaGarcia <davidlaga93@gmail.com>
2019-02-04 18:31:12 +0100

    Creating a Data Service

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,29 +1,30 @@
+import { AccountsService } from './../accounts.service';
 import { LoggingService } from './../logging.service';
 
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component} from '@angular/core';
 
-  /* Injecting the Logging Service into Components: 2
+  /* VIDEO 99.2 Injecting the Logging Service into Components
       To tell how give the service, we add a provider
   */
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  providers: [LoggingService, AccountsService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  /*Injecting the Logging Service into Components: 1
+  /* VIDEO 99.1 Injecting the Logging Service into Components
       In the construcot, naw, Angular knows what we want but no how giv us the instance.
   */
-  constructor (private loggingService: LoggingService) {}
+  constructor ( private loggingService: LoggingService,
+                private accountsService: AccountsService ) {}
+
+
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-  /* Injecting the Logging Service into Components: 3
+    this.accountsService.addAccount(accountName, accountStatus);
+
+  /* VIDEO 99.3  Injecting the Logging Service into Components
     Naw we can use our propierty 'loggingService: LoggingService'.
     So, naw angular are making the instance for as.
   */
