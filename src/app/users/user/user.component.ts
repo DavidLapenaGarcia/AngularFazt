d96b07411e2e3005047c487d3e33e0c35cc0de47
DavidLapenaGarcia <davidlaga93@gmail.com>
2019-02-08 02:21:56 +0100

    Fetching Route Parameters Reactable

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,4 @@
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -12,10 +12,27 @@ export class UserComponent implements OnInit {
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    /*
+      That wey will get tha paramatters on route.. but it will
+      fail if it try to get it in the same component, becouse
+      Angular wil not charge all the component that is it alredy charge.
+    */
     this.user = {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
+    /*
+    But doind that, the parametters are getted in a asyncro
+    wey, so it will wait for the 'event' any time, dispite if the web page
+    is refreshed or not.
+    */
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      );
   }
 
 }
