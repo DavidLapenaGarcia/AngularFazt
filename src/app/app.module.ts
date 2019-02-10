bec030575638265278c6027c414beebf2235a431
DavidLapenaGarcia <davidlaga93@gmail.com>
2019-02-10 18:42:41 +0100

    child routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,15 @@ import { ServersService } from './servers/servers.service';
 import { Routes, RouterModule} from '@angular/router';
 // DEFINE ROUTES
 const appRoutes: Routes = [
-  {path: '',                  component: HomeComponent },
-  {path: 'users',             component: UsersComponent },
-  {path: 'users/:id/:name',   component: UserComponent },
-  {path: 'servers',           component: ServersComponent},
-  {path: 'servers/:id',       component: ServerComponent},
-  {path: 'servers/:id/edit',  component: EditServerComponent}
+  {path: '',                component: HomeComponent },
+
+  {path: 'users',           component: UsersComponent, children: [
+      {path: ':id/:name',   component: UserComponent }
+  ]   },
+  {path: 'servers',       component: ServersComponent, children: [
+      {path: ':id',       component: ServerComponent},
+      {path: ':id/edit',  component: EditServerComponent}
+  ]   }
 ];
 @NgModule({
   declarations: [
